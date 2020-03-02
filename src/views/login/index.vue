8e8604723f0e3a16ff247b2e1b30095c747ff558
Programmer-new <2585710570@qq.com>
2020-03-02 21:14:57 +0800

    登录页-制作

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,28 @@
 <template>
- <div>登录</div>
+ <div class="login">
+     <el-card class='login-card'>
+         <div class="imgs">
+             <img src="../../assets/img/logo_index.png" alt="">
+         </div>
+         <!-- 表单 -->
+         <el-form>
+             <el-form-item>
+                 <el-input style="margin-top:30px" placeholder="请输入手机号"></el-input>
+             </el-form-item>
+             <el-form-item>
+                 <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+                 <el-button style="float:right " plain>验证码</el-button>
+             </el-form-item>
+            <el-form-item>
+                <el-checkbox>我已阅读同意用户协议和隐私条款</el-checkbox>
+            </el-form-item>
+            <el-form-item>
+                <el-button style="width:100%" type="primary">登录</el-button>
+            </el-form-item>
+
+         </el-form>
+     </el-card>
+ </div>
 </template>
 
 <script>
@@ -8,6 +31,23 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.login {
+    height: 100vh;
+    background-image: url('../../assets/img/login_bg.jpg');
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .imgs {
+        text-align: center;
+        img {
+            width: 200px;
+        }
+    }
+    .login-card {
+        width: 440px;
+        height: 340px;
 
+    }
+}
 </style>
